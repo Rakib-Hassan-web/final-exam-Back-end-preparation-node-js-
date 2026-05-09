dc3b068f1f8feebc18817c180737fecaf4fa8b94
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-05-09 21:45:36 +0600

    packeges instal

---
+++ dbCofig/index.js
@@ -0,0 +1,8 @@
+// const mongoose = require('mongoose');
+
+
+// const DB_Config =()=>{
+
+
+    
+// }
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
+
+
+
+
+
 
 app.listen(8000, () => {
   console.log(`server is running with port 8000`)
