2f037c1922b467b2021c7c34cb885ca5188f59ab
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-05-09 21:38:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(8000, () => {
+  console.log(`server is running with port 8000`)
+})
+
